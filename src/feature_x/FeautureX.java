afe55ab7c215664863af4e4cfdb1d843cf8d538e
Narmin <naghdaliyeva91@gmail.com>
2023-01-09 18:21:41 -0500

    First file for feauturex

--- src/feature_x/FeautureX.java
+++ src/feature_x/FeautureX.java
@@ -2,7 +2,8 @@ package feature_x;
 
 public class FeautureX {
     public static void main(String[] args) {
-        //This is the first
+        //This is the first file  for feauturex
+
 
     }
 }
